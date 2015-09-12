4140c118444541d1d7104e72e8d05be12dd1922a
Aleksei Magusev <lexmag@me.com>
2015-09-12 19:50:55 +0200

    Explicitly provide challenging path in tests

--- test/oauther_test.exs
+++ test/oauther_test.exs
@@ -3,25 +3,26 @@ defmodule OAutherTest do
 
   test "HMAC-SHA1 signature" do
     creds = OAuther.credentials(consumer_secret: "kd94hf93k423kf44", token_secret: "pfkkdhi9sl3r4s00", consumer_key: "dpf43f3p2l4k3l03", token: "nnch734d00sl2jdk")
-
-    assert signature(protocol_params(creds), creds) == "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
+    params = protocol_params(creds)
+    assert signature(params, creds, "/photos") == "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
   end
 
   test "RSA-SHA1 signature" do
     creds = OAuther.credentials(method: :rsa_sha1, consumer_secret: fixture_path("cert.pem"), consumer_key: "dpf43f3p2l4k3l03")
-
-    assert signature(protocol_params(creds), creds) == "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
+    params = protocol_params(creds)
+    assert signature(params, creds, "/photos") == "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
   end
 
   test "PLAINTEXT signature" do
     creds = OAuther.credentials(method: :plaintext, consumer_secret: "kd94hf93k423kf44", consumer_key: "dpf43f3p2l4k3l03")
 
-    assert signature([], creds) == "kd94hf93k423kf44&"
+    assert signature([], creds, "/photos") == "kd94hf93k423kf44&"
   end
 
   test "signature with query params" do
     creds = OAuther.credentials(consumer_secret: "kd94hf93k423kf44", token_secret: "pfkkdhi9sl3r4s00", consumer_key: "dpf43f3p2l4k3l03", token: "nnch734d00sl2jdk")
-    assert signature(protocol_params(creds), creds, "http://photos.example.com/photos?size=large") == "EgRKfoy8ThOLqmyWXEHgi8+OXTo="
+    params = protocol_params(creds)
+    assert signature(params, creds, "/photos?size=large") == "dzTFIxhRqhwfFqoXYgo4+hoPr2M="
   end
 
   test "Authorization header" do
@@ -42,7 +43,7 @@ defmodule OAutherTest do
 
   defp protocol_params(creds) do
     OAuther.protocol_params([file: "vacation.jpg", size: "original"], creds)
-    |> rewrite
+    |> rewrite()
   end
 
   defp rewrite(params) do
@@ -54,12 +55,13 @@ defmodule OAutherTest do
         {"oauth_timestamp", _} ->
           put_elem(param, 1, 1191242096)
 
-        _ -> param
+        _otherwise -> param
       end
     end
   end
 
-  defp signature(params, creds, query \\ "http://photos.example.net/photos") do
-    OAuther.signature("get", query, params, creds)
+  defp signature(params, creds, path) do
+    url = "http://photos.example.net" <> path
+    OAuther.signature("get", url, params, creds)
   end
 end
