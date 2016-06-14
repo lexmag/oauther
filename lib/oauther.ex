c26085ec01b07de378251b30e219c09030cd9435
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-06-14 14:39:51 +0100

    Define @spec for several functions
    * compose_key
    * read_private_key
    * base_string
    * normalize
    * normalize_pair
    * parse_url
    * parse_query_param
    * nonce
    * timestamp

--- lib/oauther.ex
+++ lib/oauther.ex
@@ -12,8 +12,11 @@ defmodule OAuther do
                :token, :token_secret, method: :hmac_sha1]
   end
 
-  @type http_header :: {String.t, any}
-  @type params :: [{String.t, any}]
+  @type pair_of_strings :: {String.t, String.t}
+  @type header :: {String.t, any}
+  @type headers :: [header]
+  @type param :: {String.t, any}
+  @type params :: [param]
 
   @spec credentials(Enumerable.t) :: Credentials.t
   def credentials(args) do
@@ -22,7 +25,7 @@ defmodule OAuther do
     end)
   end
 
-  @spec sign(String.t, String.t | URI.t, params, Credentials.t) :: [http_header]
+  @spec sign(String.t, String.t | URI.t, params, Credentials.t) :: headers
   def sign(verb, url, params, %Credentials{} = creds) do
     params = protocol_params(params, creds)
     signature = signature(verb, url, params, creds)
@@ -30,14 +33,14 @@ defmodule OAuther do
     [{"oauth_signature", signature} | params]
   end
 
-  @spec header(Enumerable.t) :: {{String.t, String.t}, [http_header]}
+  @spec header(params) :: {pair_of_strings, headers}
   def header(params) do
     {oauth_params, req_params} = Enum.partition(params, &protocol_param?/1)
 
     {{"Authorization", "OAuth " <> compose_header(oauth_params)}, req_params}
   end
 
-  @spec protocol_params(params, Credentials.t) :: [{String.t, any}]
+  @spec protocol_params(params, Credentials.t) :: params
   def protocol_params(params, %Credentials{} = creds) do
     [{"oauth_consumer_key",     creds.consumer_key},
      {"oauth_nonce",            nonce},
@@ -63,11 +66,12 @@ defmodule OAuther do
     |> Base.encode64
   end
 
-  @spec protocol_param?({String.t, any}) :: boolean
+  @spec protocol_param?(param) :: boolean
   defp protocol_param?({key, _v}) do
     String.starts_with?(key, "oauth_")
   end
 
+  @spec compose_header(nonempty_list(param | term)) :: String.t
   defp compose_header([_ | _] = params) do
     Stream.map(params, &percent_encode/1)
     |> Enum.map_join(", ", &compose_header/1)
@@ -77,17 +81,20 @@ defmodule OAuther do
     key <> "=\"" <> value <> "\""
   end
 
+  @spec compose_key(Credentials.t) :: String.t
   defp compose_key(creds) do
     [creds.consumer_secret, creds.token_secret]
     |> Enum.map_join("&", &percent_encode/1)
   end
 
+  @spec read_private_key(Path.t) :: term
   defp read_private_key(path) do
     File.read!(path)
     |> :public_key.pem_decode
     |> hd() |> :public_key.pem_entry_decode
   end
 
+  @spec base_string(String.t, String.t, params) :: String.t
   defp base_string(verb, url, params) do
     {uri, query_params} = parse_url(url)
     [verb, uri, params ++ query_params]
@@ -95,39 +102,48 @@ defmodule OAuther do
     |> Enum.map_join("&", &percent_encode/1)
   end
 
+  @spec normalize(String.t) :: String.t
   defp normalize(verb) when is_binary(verb),
     do: String.upcase(verb)
 
+  @spec normalize(URI.t) :: URI.t
   defp normalize(%URI{host: host} = uri),
     do: %{uri | host: String.downcase(host)}
 
+  @spec normalize(nonempty_list(param)) :: String.t
   defp normalize([_ | _] = params) do
     Enum.map(params, &percent_encode/1)
     |> Enum.sort
     |> Enum.map_join("&", &normalize_pair/1)
   end
 
+  @spec normalize_pair(pair_of_strings) :: String.t
   defp normalize_pair({key, value}) do
     key <> "=" <> value
   end
 
+  @spec parse_url(String.t) :: {URI.t, [pair_of_strings]}
   defp parse_url(url) do
     uri = URI.parse(url)
     {%{uri | query: nil}, parse_query_params(uri.query)}
   end
 
+  @spec parse_query_params(nil) :: []
   def parse_query_params(nil), do: []
 
+  @spec parse_query_params(params) :: [pair_of_strings]
   def parse_query_params(params) do
     URI.query_decoder(params)
     |> Enum.into([])
   end
 
+  @spec nonce() :: binary
   defp nonce() do
     :crypto.rand_bytes(32)
     |> Base.encode64
   end
 
+  @spec timestamp() :: non_neg_integer
   defp timestamp() do
     {mgsec, sec, _mcs} = :os.timestamp
 
