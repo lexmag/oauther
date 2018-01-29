523632a0c667d66327d96505ec976ab98cbf982b
Aleksei Magusev <lexmag@me.com>
2018-01-29 16:41:10 +0100

    WIP

--- test/oauther_test.exs
+++ test/oauther_test.exs
@@ -2,42 +2,83 @@ defmodule OAutherTest do
   use ExUnit.Case
 
   test "HMAC-SHA1 signature" do
-    creds = OAuther.credentials(consumer_secret: "kd94hf93k423kf44", token_secret: "pfkkdhi9sl3r4s00", consumer_key: "dpf43f3p2l4k3l03", token: "nnch734d00sl2jdk")
+    creds =
+      OAuther.credentials(
+        consumer_secret: "kd94hf93k423kf44",
+        token_secret: "pfkkdhi9sl3r4s00",
+        consumer_key: "dpf43f3p2l4k3l03",
+        token: "nnch734d00sl2jdk"
+      )
+
     params = protocol_params(creds)
     assert signature(params, creds, "/photos") == "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
   end
 
   test "RSA-SHA1 signature" do
-    creds = OAuther.credentials(method: :rsa_sha1, consumer_secret: fixture_path("private_key.pem"), consumer_key: "dpf43f3p2l4k3l03")
+    creds =
+      OAuther.credentials(
+        method: :rsa_sha1,
+        consumer_secret: fixture_path("private_key.pem"),
+        consumer_key: "dpf43f3p2l4k3l03"
+      )
+
     params = protocol_params(creds)
-    assert signature(params, creds, "/photos") == "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
+
+    assert signature(params, creds, "/photos") ==
+             "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
 
     private_key = File.read!(fixture_path("private_key.pem"))
-    creds = OAuther.credentials(method: :rsa_sha1, consumer_secret: private_key, consumer_key: "dpf43f3p2l4k3l03")
+
+    creds =
+      OAuther.credentials(
+        method: :rsa_sha1,
+        consumer_secret: private_key,
+        consumer_key: "dpf43f3p2l4k3l03"
+      )
+
     params = protocol_params(creds)
-    assert signature(params, creds, "/photos") == "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
+
+    assert signature(params, creds, "/photos") ==
+             "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
   end
 
   test "PLAINTEXT signature" do
-    creds = OAuther.credentials(method: :plaintext, consumer_secret: "kd94hf93k423kf44", consumer_key: "dpf43f3p2l4k3l03")
+    creds =
+      OAuther.credentials(
+        method: :plaintext,
+        consumer_secret: "kd94hf93k423kf44",
+        consumer_key: "dpf43f3p2l4k3l03"
+      )
 
     assert signature([], creds, "/photos") == "kd94hf93k423kf44&"
   end
 
   test "signature with query params" do
-    creds = OAuther.credentials(consumer_secret: "kd94hf93k423kf44", token_secret: "pfkkdhi9sl3r4s00", consumer_key: "dpf43f3p2l4k3l03", token: "nnch734d00sl2jdk")
+    creds =
+      OAuther.credentials(
+        consumer_secret: "kd94hf93k423kf44",
+        token_secret: "pfkkdhi9sl3r4s00",
+        consumer_key: "dpf43f3p2l4k3l03",
+        token: "nnch734d00sl2jdk"
+      )
+
     params = protocol_params(creds)
     assert signature(params, creds, "/photos?size=large") == "dzTFIxhRqhwfFqoXYgo4+hoPr2M="
   end
 
   test "Authorization header" do
-    {header, req_params} = OAuther.header [
-      {"oauth_consumer_key",     "dpf43f3p2l4k3l03"},
-      {"oauth_signature_method", "PLAINTEXT"},
-      {"oauth_signature",        "kd94hf93k423kf44&"},
-      {"build",                  "Luna Park"}
-    ]
-    assert header == {"Authorization", ~S(OAuth oauth_consumer_key="dpf43f3p2l4k3l03", oauth_signature_method="PLAINTEXT", oauth_signature="kd94hf93k423kf44%26")}
+    {header, req_params} =
+      OAuther.header([
+        {"oauth_consumer_key", "dpf43f3p2l4k3l03"},
+        {"oauth_signature_method", "PLAINTEXT"},
+        {"oauth_signature", "kd94hf93k423kf44&"},
+        {"build", "Luna Park"}
+      ])
+
+    assert header ==
+             {"Authorization",
+              ~S(OAuth oauth_consumer_key="dpf43f3p2l4k3l03", oauth_signature_method="PLAINTEXT", oauth_signature="kd94hf93k423kf44%26")}
+
     assert req_params == [{"build", "Luna Park"}]
   end
 
@@ -58,9 +99,10 @@ defmodule OAutherTest do
           put_elem(param, 1, "kllo9940pd9333jh")
 
         {"oauth_timestamp", _} ->
-          put_elem(param, 1, 1191242096)
+          put_elem(param, 1, 1_191_242_096)
 
-        _otherwise -> param
+        _otherwise ->
+          param
       end
     end
   end
