eb01abd06164fceb677d657c2b8dfffdb3177a09
Joao Bernardo <jb.amaro95@gmail.com>
2021-11-13 23:35:38 +0000

    Wrap signature/4 tests in a describe block
    To improve test readability.

--- test/oauther_test.exs
+++ test/oauther_test.exs
@@ -1,83 +1,85 @@
 defmodule OAutherTest do
   use ExUnit.Case
 
-  test "HMAC-SHA1 signature" do
-    creds =
-      OAuther.credentials(
-        consumer_secret: "kd94hf93k423kf44",
-        token_secret: "pfkkdhi9sl3r4s00",
-        consumer_key: "dpf43f3p2l4k3l03",
-        token: "nnch734d00sl2jdk"
-      )
+  describe "signature/4" do
+    test "HMAC-SHA1 signature" do
+      creds =
+        OAuther.credentials(
+          consumer_secret: "kd94hf93k423kf44",
+          token_secret: "pfkkdhi9sl3r4s00",
+          consumer_key: "dpf43f3p2l4k3l03",
+          token: "nnch734d00sl2jdk"
+        )
 
-    params = protocol_params(creds)
-    assert signature(params, creds, "/photos") == "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
-  end
+      params = protocol_params(creds)
+      assert signature(params, creds, "/photos") == "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
+    end
 
-  test "HMAC-SHA256 signature" do
-    creds =
-      OAuther.credentials(
-        method: :hmac_sha256,
-        consumer_secret: "kd94hf93k423kf44",
-        token_secret: "pfkkdhi9sl3r4s00",
-        consumer_key: "dpf43f3p2l4k3l03",
-        token: "nnch734d00sl2jdk"
-      )
+    test "HMAC-SHA256 signature" do
+      creds =
+        OAuther.credentials(
+          method: :hmac_sha256,
+          consumer_secret: "kd94hf93k423kf44",
+          token_secret: "pfkkdhi9sl3r4s00",
+          consumer_key: "dpf43f3p2l4k3l03",
+          token: "nnch734d00sl2jdk"
+        )
 
-    params = protocol_params(creds)
-    assert signature(params, creds, "/photos") == "WVPzl1j6ZsnkIjWr7e3OZ3jkenL57KwaLFhYsroX1hg="
-  end
+      params = protocol_params(creds)
+      assert signature(params, creds, "/photos") == "WVPzl1j6ZsnkIjWr7e3OZ3jkenL57KwaLFhYsroX1hg="
+    end
 
-  test "RSA-SHA1 signature" do
-    creds =
-      OAuther.credentials(
-        method: :rsa_sha1,
-        consumer_secret: fixture_path("private_key.pem"),
-        consumer_key: "dpf43f3p2l4k3l03"
-      )
+    test "RSA-SHA1 signature" do
+      creds =
+        OAuther.credentials(
+          method: :rsa_sha1,
+          consumer_secret: fixture_path("private_key.pem"),
+          consumer_key: "dpf43f3p2l4k3l03"
+        )
 
-    params = protocol_params(creds)
+      params = protocol_params(creds)
 
-    assert signature(params, creds, "/photos") ==
-             "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
+      assert signature(params, creds, "/photos") ==
+               "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
 
-    private_key = File.read!(fixture_path("private_key.pem"))
+      private_key = File.read!(fixture_path("private_key.pem"))
 
-    creds =
-      OAuther.credentials(
-        method: :rsa_sha1,
-        consumer_secret: private_key,
-        consumer_key: "dpf43f3p2l4k3l03"
-      )
+      creds =
+        OAuther.credentials(
+          method: :rsa_sha1,
+          consumer_secret: private_key,
+          consumer_key: "dpf43f3p2l4k3l03"
+        )
 
-    params = protocol_params(creds)
+      params = protocol_params(creds)
 
-    assert signature(params, creds, "/photos") ==
-             "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
-  end
+      assert signature(params, creds, "/photos") ==
+               "cyZ9hTJnRfkOnF5+OzxXWKKG+hRY+/esxdQAluJem1RlHkZQRsFEevOS5x+A1ZoS+aYlTU3xdHkEKIb/+xuqaavAUFVaIF/5448XsXqSTJomvpoC1c7yw5ArNZnPRLYwK3XYHaIr5FHXbiCG/ze093i2MpsusQU6Shn8lGJNMWE="
+    end
 
-  test "PLAINTEXT signature" do
-    creds =
-      OAuther.credentials(
-        method: :plaintext,
-        consumer_secret: "kd94hf93k423kf44",
-        consumer_key: "dpf43f3p2l4k3l03"
-      )
+    test "PLAINTEXT signature" do
+      creds =
+        OAuther.credentials(
+          method: :plaintext,
+          consumer_secret: "kd94hf93k423kf44",
+          consumer_key: "dpf43f3p2l4k3l03"
+        )
 
-    assert signature([], creds, "/photos") == "kd94hf93k423kf44&"
-  end
+      assert signature([], creds, "/photos") == "kd94hf93k423kf44&"
+    end
 
-  test "signature with query params" do
-    creds =
-      OAuther.credentials(
-        consumer_secret: "kd94hf93k423kf44",
-        token_secret: "pfkkdhi9sl3r4s00",
-        consumer_key: "dpf43f3p2l4k3l03",
-        token: "nnch734d00sl2jdk"
-      )
+    test "signature with query params" do
+      creds =
+        OAuther.credentials(
+          consumer_secret: "kd94hf93k423kf44",
+          token_secret: "pfkkdhi9sl3r4s00",
+          consumer_key: "dpf43f3p2l4k3l03",
+          token: "nnch734d00sl2jdk"
+        )
 
-    params = protocol_params(creds)
-    assert signature(params, creds, "/photos?size=large") == "dzTFIxhRqhwfFqoXYgo4+hoPr2M="
+      params = protocol_params(creds)
+      assert signature(params, creds, "/photos?size=large") == "dzTFIxhRqhwfFqoXYgo4+hoPr2M="
+    end
   end
 
   test "Authorization header" do
